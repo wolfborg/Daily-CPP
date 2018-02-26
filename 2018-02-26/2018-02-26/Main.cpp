ad5cc4808c135ddcd067c8f47b2e6fefd4761cae
Derek Chaplin <wolfborg@users.noreply.github.com>
2018-02-26 15:35:08 -0500

    OpenGL Setup

--- 2018-02-26/2018-02-26/Main.cpp
+++ 2018-02-26/2018-02-26/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-using namespace std;
+#include <GL/glew.h>
 
-int main() {
-	cout << "Hello World" << endl;
+int main(int argc, char** argv) {
+	std::cout << "Hello World" << std::endl;
+	return 0;
 }
