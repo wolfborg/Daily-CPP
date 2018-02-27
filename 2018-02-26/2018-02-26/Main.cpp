a7311dc0ab91e237c5fe7d34a5089f77f7ff9731
Derek Chaplin <wolfborg@users.noreply.github.com>
2018-02-26 19:36:18 -0500

    OpenGL Display Setup

--- 2018-02-26/2018-02-26/Main.cpp
+++ 2018-02-26/2018-02-26/Main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <GL/glew.h>
+#include "display.h"
 
-int main(int argc, char** argv) {
-	std::cout << "Hello World" << std::endl;
+int main(int argc, char** argv)
+{
+	Display display(800, 600, "Hello World!");
+	while (!display.IsClosed())
+	{
+		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+		display.Update();
+	}
 	return 0;
 }
